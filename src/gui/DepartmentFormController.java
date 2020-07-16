df2857c9c424f5f110c47c10063ba9d24294c15b
MariaCaroll <carolfavola@gmail.com>
2020-07-16 18:39:44 -0300

    Controle do Fomularioi do Departameto

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,53 @@
+package gui;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartmentFormController implements Initializable{
+
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtNome;
+	
+	@FXML
+	private Label labelErrorNome;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	@FXML
+	private void onBtSaveAction() {
+		System.out.println("Sava");
+	}
+	
+	@FXML
+	private void onBtCancelAction() {
+		System.out.println("cancela");
+	}
+	
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtNome, 30);
+		
+	}
+
+}
