f582fba0381e7df644f908c51a993a29d2c9a9a5
MariaCaroll <carolfavola@gmail.com>
2020-07-16 19:05:17 -0300

    Metodo Salvar

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -4,14 +4,25 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import model.entities.Department;
+import model.services.DepartmentService;
 
-public class DepartmentFormController implements Initializable{
+public class DepartmentFormController implements Initializable {
+	
+	private Department entity;
+	
+	private DepartmentService service;
 
 	@FXML
 	private TextField txtId;
@@ -28,11 +39,41 @@ public class DepartmentFormController implements Initializable{
 	@FXML
 	private Button btCancel;
 	
-	@FXML
-	private void onBtSaveAction() {
-		System.out.println("Sava");
+	public void setDepartmetService(DepartmentService service) {
+		this.service = service;
 	}
 	
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+	
+	@FXML
+	private void onBtSaveAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException("Entity esta vazia");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service esta vazia");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+		} catch (DbException e) {
+			Alerts.showAlert("Error ao Salvar", null, e.getMessage(), AlertType.ERROR);
+			
+		}
+		
+	}
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtNome.getText());
+		return obj;
+	}
+
 	@FXML
 	private void onBtCancelAction() {
 		System.out.println("cancela");
@@ -48,6 +89,16 @@ public class DepartmentFormController implements Initializable{
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtNome, 30);
 		
+	}
+	
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entidade Vazia");
+		} else {
+			txtId.setText(String.valueOf(entity.getId()));
+			txtNome.setText(entity.getName());
+		}
+	
 	}
 
 }
